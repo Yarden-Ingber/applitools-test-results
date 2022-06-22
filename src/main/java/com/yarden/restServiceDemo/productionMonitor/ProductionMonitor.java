87f1f4a786b5401377947fced15df328f6edb416
Yarden.Ingber <yarden.ingber@applitools.com>
2022-06-22 23:07:10 +0300

    remove non relevant sites from ignore production monitor list

--- src/main/java/com/yarden/restServiceDemo/productionMonitor/ProductionMonitor.java
+++ src/main/java/com/yarden/restServiceDemo/productionMonitor/ProductionMonitor.java
@@ -85,7 +85,7 @@ public class ProductionMonitor extends TimerTask {
         for (String domainSite : domainsSitesList) {
             String domain = domainSite.split(",")[0];
             String site = domainSite.split(",")[1];
-            if (! ( domain.contains("***") || site.equals("Itaitest") || site.equals("Yotamdemo") || domain.contains("gehceyes") || site.equals("Jpmc") || site.equals("Jnj") || site.equals("Vha")) ) {
+            if (! ( domain.contains("***") || site.equals("Itaitest") || site.equals("Yotamdemo")) ) {
                 domain = domain + "/api/admin/userinfo";
                 URL endpoint = new URL(domain);
                 JSONObject productionMonitorEventJson = new JSONObject();
